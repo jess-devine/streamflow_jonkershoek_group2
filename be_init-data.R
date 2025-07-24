8e1b3651915e3efe927c4cb63a968322072b189d
jess-devine <jesskdevine@gmaile.com>
2025-07-24 14:54:23 +0200

    VPD

--- be_init-data.R
+++ be_init-data.R
@@ -34,3 +34,33 @@ hist(streamflow, main = "", las = 1)
 hist(log(streamflow), main = "", las = 1)
 streamflow = log(streamflow)
 summary(streamflow)
+
+##########################################################################
+
+# Data for vapor deficit (Jess) 
+
+names(hdat2014)
+temperature <- hdat2014$Air.Temperature[5000:6000]
+RH <- hdat2014$Relative.Humidity[5000:6000]
+
+calculate_vpd <- function(temp_C, RH) {
+  # Saturation vapor pressure (es) in kPa 
+  es <- 0.6108 * exp((17.27 * temp_C) / (temp_C + 237.3))
+  # Actual vapor pressure (ea) in kPa
+  ea <- (RH / 100) * es
+  # Vapor Pressure Deficit (VPD)
+  vpd <- es - ea
+  return(vpd)
+}
+
+# Calculate VPD for the entire dataset
+hdat2014$VPD <- calculate_vpd(hdat2014$Air.Temperature, hdat2014$Relative.Humidity)
+
+# VPD for subset
+VPD <- hdat2014$VPD[5000:6000]
+
+par(mfrow = c(3, 1)) 
+plot(streamflow~temperature)
+plot(streamflow~RH)
+plot(streamflow ~ VPD)
+
---
+++ jd_ssm-with-VPD.R
@@ -0,0 +1,98 @@
+# SSM with VPD 
+
+### n = 1000
+### last 200 NA
+### streamflow = log(streamflow)
+
+
+n <- length(streamflow)
+streamflow_new <- streamflow
+streamflow_new[800:n] <- NA
+
+
+### --- prepare rain variable
+
+sel <- 5000:6000
+rain <- hdat2014$Rainfall[sel]
+
+hist(rain, las = 1, main = "")
+
+par(mfrow = c(3, 1))
+plot.ts(rain)
+plot.ts(streamflow)
+plot.ts(VPD)
+
+### --- Fit JAGS model: ssm plus rain
+
+data <- list(y = streamflow_new,
+             n = length(streamflow),      ## data
+            # rain = log(rain +1),
+             VPD = log(VPD +1),
+             x_ic = mean(streamflow, na.rm = TRUE),
+             tau_ic = 100, ## initial condition prior
+             a_obs = 1,
+             r_obs = 1,           ## obs error prior
+             a_add = 1,
+             r_add = 1            ## process error prior
+)
+
+
+nchain = 3
+init <- list()
+for(i in 1:nchain){
+  ##  y.samp = sample(y,length(y),replace=TRUE)
+  init[[i]] <- list(tau_add = 1 / var(diff(streamflow), na.rm= TRUE),  ## initial guess on process precision
+                    tau_obs = 5 / var(streamflow, na.rm= TRUE),      ## initial guess on obs precision
+                    beta_VPD = rnorm(1, 0, 1))
+}
+
+j.model   <- jags.model (file = "jd_ssm-VPD.jags",
+                         data = data,
+                         inits = init,
+                         n.chains = 3)
+
+
+## burn-in
+jags.out   <- coda.samples (model = j.model,
+                            variable.names = c("tau_add","tau_obs", "beta_VPD"),
+                            n.iter = 3000)
+
+jags.out   <- coda.samples (model = j.model,
+                            variable.names = c("tau_add","tau_obs", "beta_VPD"),
+                            n.iter = 3000)
+jags.out   <- coda.samples (model = j.model,
+                            variable.names = c("tau_add","tau_obs", "beta_VPD"),
+                            n.iter = 3000)
+
+plot(jags.out)
+#dic.samples(j.model, 2000)
+
+
+jags.out   <- coda.samples (model = j.model,
+                            variable.names = c("x","tau_add","tau_obs"),
+                            n.iter = 10000)
+
+
+### ---- plot observed and forecast
+par(mfrow = c(1, 1)) 
+tt = seq(1, length(streamflow))       ## adjust to zoom in and out
+out <- as.matrix(jags.out)         ## convert from coda to matrix  
+x.cols <- grep("^x",colnames(out)) ## grab all columns that start with the letter x
+ci <- apply(out[,x.cols],2,quantile,c(0.025,0.5,0.975)) ## model was fit on log scale
+
+plot(tt, ci[2,], type = 'n', ylab = "log(streamflow)", ylim = c(-5, 3), 
+     las = 1, xlab = "time (hourly)")
+## adjust x-axis label to be monthly if zoomed
+# if(diff(time.rng) < 100){ 
+#   axis.Date(1, at=seq(time[time.rng[1]],time[time.rng[2]],by='month'), format = "%Y-%m")
+# }
+ecoforecastR::ciEnvelope(tt, ci[1,], ci[3,], 
+                         col = ecoforecastR::col.alpha("lightBlue",0.75))
+points(tt, streamflow, pch="+", cex=0.5)
+
+pred <- apply(out[,x.cols], 2, mean)
+points(tt, pred, col = "red", pch = 20, cex = 0.3)
+
+legend("topleft", bty = "n", col = c("black", "red"), 
+       legend = c("observed", "predicted"), lwd = 2)
+
